6f0ae1352af12225804a08b61ed3cbb0f8569bda
Somogyi Ervin <ervin.somogyi@gmail.com>
2025-09-29 21:08:45 +0200

    fragments

--- app/src/components.js
+++ app/src/components.js
@@ -21,11 +21,17 @@ const Menu = () => {
     <main className="menu">
       <h2>Our menu</h2>
       {hasPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizza={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizza={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Plese come back later! :)</p>
       )}
