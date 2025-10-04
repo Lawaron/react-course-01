f58e72725da59cb2bc060702d40aaa4ae5a85d7b
Somogyi Ervin <ervin.somogyi@gmail.com>
2025-10-04 18:28:26 +0200

    experiences with State

---
+++ app/src/App.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const Step = ({ step, item, stepHandler }) => {
+  const handleClick = () => {
+    stepHandler(item);
+  };
+
+  return (
+    <div
+      style={{ cursor: "pointer" }}
+      className={`${step >= item && "active"}`}
+      onClick={handleClick}
+    >
+      {item}
+    </div>
+  );
+};
+
+const Button = ({ text, clickHandler }) => (
+  <button
+    style={{ backgroundColor: "#7950f2", color: "#fff" }}
+    onClick={clickHandler}
+  >
+    {text}
+  </button>
+);
+
+const App = () => {
+  const [step, setStep] = useState(1);
+
+  const handlePrevious = () => {
+    if (step > 1) setStep(step - 1);
+  };
+
+  const handleNext = () => {
+    setStep((s) => (s === 3 ? s : s + 1));
+  };
+
+  const handleStep = (s) => {
+    setStep(s);
+  };
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        {messages.map((_, index) => (
+          <Step
+            step={step}
+            item={index + 1}
+            stepHandler={handleStep}
+            key={index}
+          />
+        ))}
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <Button text="Previous" clickHandler={handlePrevious} />
+        <Button text="Next" clickHandler={handleNext} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
