b57c90944637b9241c73009f43f6b9576750f740
Somogyi Ervin <ervin.somogyi@gmail.com>
2025-11-18 13:24:30 +0100

    Custom Hooks

--- app/src/components/Search.js
+++ app/src/components/Search.js
@@ -1,11 +1,31 @@
-const Search = ({ query, setQuery }) => (
-  <input
-    className="search"
-    type="text"
-    placeholder="Search movies..."
-    value={query}
-    onChange={(e) => setQuery(e.target.value)}
-  />
-);
+import { useEffect, useRef } from "react";
+
+const Search = ({ query, setQuery }) => {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    const callback = (e) => {
+      if (e.key !== "Enter" || document.activeElement === inputEl.current)
+        return;
+
+      inputEl.current.focus();
+    };
+
+    document.addEventListener("keydown", callback);
+
+    return () => document.removeEventListener("keydown", callback);
+  }, []);
+
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
+    />
+  );
+};
 
 export default Search;
